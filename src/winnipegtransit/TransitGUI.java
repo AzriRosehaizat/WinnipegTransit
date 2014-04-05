364783c050d969b7ce642db49596967c256447c6
Owen Peterson <owen@MSI-Laptop.oapeter>
2014-04-05 06:58:11 -0500

    cleaned up unused inmports in gui clasS

--- src/winnipegtransit/TransitGUI.java
+++ src/winnipegtransit/TransitGUI.java
@@ -5,14 +5,8 @@
  */
 
 package winnipegtransit;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Date;
-import java.net.URL;
-import java.net.MalformedURLException;
-import org.json.JSONObject;
 import org.json.JSONException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
